a481f2f6676041d288bf046e2a8248b388328f58
munibqazi12 <munibqazi770@gmail.com>
2024-10-07 20:47:19 +0500

    New updates are added

--- resources/js/Pages/Admin/seo/CreateEdit.vue
+++ resources/js/Pages/Admin/seo/CreateEdit.vue
@@ -351,7 +351,7 @@ const form = useForm({
   ],
   slug: props.seo?.slug || "",
   meta_title: props.seo?.meta_title || "",
-  meta_description: props.seo?.meta_description || "",
+  meta_description: props.seo?.meta_description || "hi",
   status: props.seo?.status ?? "",
   p_tag: props.seo?.p_tag || "",
   h1_tag: props.seo?.h1_tag || "",
